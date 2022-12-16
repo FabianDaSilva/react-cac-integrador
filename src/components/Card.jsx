46121205ae2803e8877c40d48c9d8a663e08082a
Fabian Da Silva <fabian.da.silva@hotmail.com>
2022-12-15 23:32:08 -0300

    feat: :white_check_mark: Add modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,37 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useState } from "react";
+
+import { useMovieBanner } from "../hooks";
+
 const Card = ({ oneMovie }) => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const movie = useMovieBanner("movie");
+
   return (
     <>
       <img
         alt={oneMovie.title}
         className="row__movies--img"
         src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`}
+        onClick={toggleModal}
       />
+
+      {modal && (
+        <div className="modal">
+          <div className="overlay" onClick={toggleModal}>
+            <div className="modal-content">
+              <h1> {movie?.title || movie?.name || movie?.orignal_name}</h1>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
